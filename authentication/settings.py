885b7787480cda5741821229621c424162439d79
Chibuike Ewenike <sopewenike@yahoo.com>
2025-06-14 10:19:17 -0700

    added bytealpha.online to allowed host settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -65,7 +65,9 @@ CORS_ALLOWED_ORIGINS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.53.36.102",
+ALLOWED_HOSTS = [
+    "bytealpha.online",
+    "13.53.36.102",
     "authsysbackend-cfqp.onrender.com",
     "localhost",
     "127.0.0.1",
